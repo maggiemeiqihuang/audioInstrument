9ed29bd5fb2940fb96e89d447b5adfeba2b9f50a
maggiemeiqihuang <maggiemeiqihuang@gmail.com>
2026-07-30 12:31:45 +1000

    test note
    we made a test button play a single note

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-document.body.style.backgroundColor = "red";
+// document.body.style.backgroundColor = "red";
+// find my test button
+const testButton = document.getElementById("test-button");
+
+// create intrument and connect to audio
+const Synth = new Tone.Synth().toDestination();
+
+
+// do something when we click that button
+testButton.addEventListener("click" , playTestNote);
+
+function playTestNote(){
+    Synth.triggerAttackRelease("C4", "8n");
+}
